588e9d9801d07897eb6babe1dc1779e3f5b02235
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-04-28 13:34:05 +0900

    Update seats.controller.ts

--- src/seats/seats.controller.ts
+++ src/seats/seats.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { SeatsService } from './seats.service';
 
 @Controller('seats')
-export class SeatsController {}
+export class SeatsController {
+  constructor(private readonly seatsService: SeatsService) {}
+
+  @Get('theater/:id')
+  async getSeatsByTheaterID(@Param('id') id: number) {
+    return await this.seatsService.getSeatsByTheaterID(id);
+  }
+
+  @Get('user/:username')
+  async getBookedSeatsByUsername(@Param('username') username: string) {
+    return await this.seatsService.getBookedSeatsByUsername(username);
+  }
+}
