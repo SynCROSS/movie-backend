e7e8cfee44a25edae6ae934494385bd92b09b7bc
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-04-29 10:11:15 +0900

    Update tv.service.ts

--- src/tv/tv.service.ts
+++ src/tv/tv.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import axios from 'axios';
 import {
-  generateGenreURL,
+  // generateGenreURL,
   generateMovieOrSeriesURL,
   generateTrendingURL,
 } from 'src/lib/generateURL';
