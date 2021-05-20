0c75b37697980f454af96e80941dd4a34cb5e217
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-05-20 22:35:34 +0900

    For Using @Res()

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Post, UseGuards, Req, Get } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Post, UseGuards, Req, Get, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
@@ -9,14 +9,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(JwtAuthGuard)
-  @Get('profile')
+  @Get('check')
   checkLoggedIn(@Req() req: Request) {
     return req.user;
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Req() req: Request) {
-    return this.authService.login(req.body);
+  async login(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    return await this.authService.login(req.body, res);
   }
 }
