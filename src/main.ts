a7a4e36e28bac3b8887710882e8d90becfeb0b8f
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-05-21 16:53:36 +0900

    removed cors package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,12 +13,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.use(
-    cors({
-      origin: 'http://localhost:3000',
-      credentials: false, // default
-    }),
-  );
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: false,
+  });
   await app.listen(4000);
 }
 bootstrap();
