58efc002dd4c8f9a6d59f39de0ce59d0e2e0c43d
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-06-04 15:03:05 +0900

    Make Code Short

--- src/lib/isValid.ts
+++ src/lib/isValid.ts
@@ -1,35 +1,19 @@
 export const isValidMedia = (media: string): boolean => {
-  if (!/movie|tv/.exec(media)) {
-    return false;
-  }
-  return true;
+  return !!/movie|tv/.exec(media);
 };
 
-export const isValidTarget = (target: string | number): boolean => {
-  if (!/\d+|top_rated|popular|upcoming/i.exec(`${target}`)) {
-    return false;
-  }
-
-  return true;
+export const isValidCategory = (target: string | number): boolean => {
+  return !!/top_rated|popular|upcoming/i.exec(`${target}`);
 };
 
 export const isValidTimeWindow = (time_window: string): boolean => {
-  if (!/week|day/.exec(time_window)) {
-    return false;
-  }
-  return true;
+  return !!/week|day/.exec(time_window);
 };
 
 export const isValidInfoType = (info_type: string): boolean => {
-  if (!/recommendations|reviews|similar/.exec(info_type)) {
-    return false;
-  }
-  return true;
+  return !!/recommendations|reviews|similar/.exec(info_type);
 };
 
 export const isValidID = (id: number): boolean => {
-  if (!/\d+/.exec(`${id}`)) {
-    return false;
-  }
-  return true;
+  return !!/\d+/.exec(`${id}`);
 };
