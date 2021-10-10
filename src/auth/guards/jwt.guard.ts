5a3d0934f2549c142980c654190499b447ab1985
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-10 20:40:36 +0900

    Remove Comment

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -5,9 +5,10 @@ import { TokenExpiredError } from 'jsonwebtoken';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, user, info: Error) {
-    // if (info instanceof TokenExpiredError) {
-    //   console.log('token expired');
-    // }
+    if (info instanceof TokenExpiredError) {
+      console.log('token expired');
+      console.error(err);
+    }
     return user;
   }
 }
