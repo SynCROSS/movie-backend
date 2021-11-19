7a23c5ca049f624067f748cdb3c422af79637dad
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-11-19 09:33:06 +0900

    Add 'express-session'

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { config } from 'dotenv';
+import * as session from 'express-session';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+
+  app.use(cookieParser(process.env.SECRET_KEY));
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.SECRET_KEY,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+        signed: true,
+      },
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
